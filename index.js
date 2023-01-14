bcfd2f9e63c1fb2638cbe61dce27bd149940110f
Maria Turunen <maria.2.turunen@tuni.fi>
2023-01-14 13:08:15 +0200

    index.js ADD: Health check for render and package.json ADD: scripts: start

--- index.js
+++ index.js
@@ -4,6 +4,14 @@ const app = require('./app');
 
 const PORT = process.env.PORT || 5000;
 
+app.get('/health', (req, res) => {
+  if (res) {
+    res.status(200).send('OK');
+  } else {
+    res.status(404).send('Not Found');
+  }
+});
+
 // Start listener For console to see if backend is right port
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
