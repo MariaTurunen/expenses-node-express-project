32ae42722fb9a60acbeae15b5a7680634dca6f9f
Maria Turunen <maria.2.turunen@tuni.fi>
2023-01-15 16:49:41 +0200

    ADD: experiment basic HTML for REST calls

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 // use express framework
 const express = require('express');
+const cors = require('cors');
 // Use router
 const expensesRouter = require('./routes/expenses');
 
 const app = express();
 app.use(express.json());
+app.use(cors({ origin: ['http://localhost:5000', 'https://expenses-node-express-api.onrender.com'] }));
 // use controller
 app.use('/api/expenses', expensesRouter);
 
